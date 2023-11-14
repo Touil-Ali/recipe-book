6e4de69196099d52ef43b72bf4c8aaf026f70b0a
Ali <96635567+Touil-Ali@users.noreply.github.com>
2023-11-15 00:05:46 +0100

    update ui

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -13,19 +13,14 @@ export default {
 <template>
 
   <section>
-    <!-- Container -->
-    <div class="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
-      <!-- Component -->
-      <div class="flex flex-col items-center">
-        <!-- Content -->
-        <div class="mb-12 grid gap-4 sm:grid-cols-2 sm:justify-items-stretch md:mb-16 md:grid-cols-3 lg:mb-20 lg:gap-6">
+
+
+
           <!-- Item -->
           <a href="#" class="flex flex-col gap-4 rounded-md bg-[#f2f2f7] px-4 py-8 md:p-4">
             <div class="relative">
-              <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/64772c025e9047a330ad448e_Rectangle%2034.png" alt="" class="inline-block h-60 w-full rounded-md object-cover" />
-              <div class="absolute bottom-[-10%] right-[8%] flex h-16 w-16 items-center justify-center rounded-full border-4 border-solid border-[#f2f2f7] bg-black">
-                <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/647e0b7ecf55e2b1c5295de4_Vector.svg" alt="" class="inline-block" />
-              </div>
+              <img :src="recipe.image" alt="" class="inline-block h-60 w-full rounded-md object-cover" />
+
             </div>
             <div class="flex w-full flex-col gap-5">
               <div class="rounded-md bg-[#f2f2f7] px-2 py-1.5">
@@ -35,7 +30,6 @@ export default {
               <!-- Divider -->
               <div class="h-px w-full bg-black"></div>
               <div class="flex items-center">
-                <img :src="recipe.image" alt="" class="mr-4 inline-block h-8 w-8 rounded-full" />
                 <div class="flex flex-col md:flex-row md:items-center">
                   <h6 class="text-base font-bold">Jessica Smith</h6>
                   <p class="mx-2 hidden text-sm text-[#636262] lg:block">-</p>
@@ -46,11 +40,8 @@ export default {
           </a>
 
 
-        </div>
-        <!-- Button -->
-        <a href="#" class="rounded-md bg-black px-6 py-3 font-semibold text-white">Check All&nbsp;Reviews</a>
-      </div>
-    </div>
+
+
   </section>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 
 
 <template>
-  <h1>Welcome to Moroccan Recipes</h1>
-  <p>Here you will find a collection of Moroccan recipes.</p>
-  <p>Click on a recipe to see the details.</p>
+<navbar></navbar>
+  <div class="flex flex-col items-center">
+
+  <h2 class="max-w-3xl text-center text-3xl font-bold md:text-5xl">Welcome to Moroccan Recipes</h2>
+  <p class="mt-4 text-[#636262]">Here you will find a collection of Moroccan recipes.</p>
+  <p class="mb-12 mt-4 text-[#636262]">Click on a recipe to see the details.</p>
+</div>
+
   <div class="recipe-list">
 
     <div v-if="recipes.length > 0">
-
+      <!-- Container -->
+      <div class="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+        <!-- Component -->
+        <div class="flex flex-col items-center">
+          <!-- Content -->
+          <div class="mb-12 grid gap-4 sm:grid-cols-2 sm:justify-items-stretch md:mb-16 md:grid-cols-3 lg:mb-20 lg:gap-6">
     <router-link
         v-for="recipe in recipes"
         :key="recipe.id"
@@ -15,7 +25,13 @@
     >
       <recipe-card :recipe="recipe" />
     </router-link>
+      </div>
+          <!-- Button -->
+          <a href="#" class="rounded-md bg-black px-6 py-3 font-semibold text-white">Check All&nbsp;Reviews</a>
 
+        </div>
+
+      </div>
 
     </div>
     <div v-else>
@@ -28,10 +44,11 @@
 import RecipeList from "@/components/RecipeList.vue";
 import RecipeCard from "@/components/RecipeCard.vue";
 import json from "@/data/recipes.json";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "Home",
-  components: {RecipeCard, RecipeList},
+  components: {Navbar, RecipeCard, RecipeList},
   data() {
     return {
       recipes: json.recipes,
