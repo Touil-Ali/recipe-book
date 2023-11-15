484fd49a116e6ca561a01107be204aca9bc3b1e3
Ali <96635567+Touil-Ali@users.noreply.github.com>
2023-11-15 20:01:41 +0100

    update ui v2

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import VueKinesisEsm from "vue-kinesis";
 
 const app = createApp(App);
 
 app.use(router);
-
+app.use(VueKinesisEsm);
 app.mount('#app');
